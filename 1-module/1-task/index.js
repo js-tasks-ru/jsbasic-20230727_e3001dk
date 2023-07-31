018877cfbcc1790964abbe3782fe0efb4366fdea
e.dyakov <e3001dk@gmail.com>
2023-07-31 23:08:09 +0300

    решил задачу "вычислить факториал"

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
-function factorial(n) {
-  // ваш код...
+function factorial (n) {
+  if (n) {
+    let result = n;
+    for (let i = 1; i < n; i++) {
+      result *= n - i;
+    }
+    return result;
+  }
+  return 1;
 }
