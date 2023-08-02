24b9de2c9b7be0b8a81193e91eb5bed742ebadcc
e.dyakov <e3001dk@gmail.com>
2023-08-02 23:55:27 +0300

    решил задачу "проверка на спам"

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
-function checkSpam(str) {
-  // ваш код...
+function checkSpam (str) {
+  str = str.toLowerCase();
+  const words = ['1xbet', 'xxx'];
+
+  for (let word of words) {
+    if (str.includes(word)) {
+      return true;
+    }
+  }
+  return false;
 }
