5f6c7c03ed5f2ea17551ef7e29b2f79bac10e1eb
e.dyakov <e3001dk@gmail.com>
2023-08-08 01:39:03 +0300

    решил задачу "функция showSalary(users, age)"

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,7 @@
-function showSalary(users, age) {
-  // ваш код...
+function showSalary (users, age) {
+  const correctAgeUsers = users.filter((user) => user.age <= age);
+
+  const usersSalaries = correctAgeUsers.map((user) => `${user.name}, ${user.balance}`);
+
+  return usersSalaries.join('\n');
 }
