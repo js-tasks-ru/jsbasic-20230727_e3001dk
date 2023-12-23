f6fcfe02da948c1c51e60f9f9e0cb3eeca120b6b
e.dyakov <e3001dk@gmail.com>
2023-12-23 22:59:00 +0200

    решил задачу: учебный проект: список товаров

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,61 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+
+    this.createProductGrid();
+  }
+
+  createProductGrid() {
+    const productGrid = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner"></div>
+      </div>
+    `);
+
+    this.inner = productGrid.querySelector('.products-grid__inner');
+
+    this.products.forEach(product => this.inner.append(new ProductCard(product).elem));
+
+    this.elem = productGrid;
+  }
+
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+
+    const filteredProducts = this.products.filter(product => {
+
+      for (const key in this.filters) {
+        const value = this.filters[key];
+
+        if (key === 'noNuts') {
+          if (value && product.nuts) {
+            return false;
+          }
+        }
+
+        if (key === 'vegeterianOnly') {
+          if (value && !product.vegeterian) {
+            return false;
+          }
+        }
+
+        if (key === 'maxSpiciness') {
+          if (value < product.spiciness) {
+            return false;
+          }
+        }
+
+        if (key === 'category') {
+          if (value && value !== product.category) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    });
+
+    this.inner.innerHTML = '';
+    filteredProducts.forEach(product => this.inner.append(new ProductCard(product).elem));
   }
 }
