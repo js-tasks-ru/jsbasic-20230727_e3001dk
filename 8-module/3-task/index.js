8a2bf560eccacdbc85f8b57130d03cb9e496609b
e.dyakov <e3001dk@gmail.com>
2023-12-26 10:45:11 +0200

    решил задачу: учебный проект: корзина, часть 1

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,52 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {
+      return;
+    }
+
+    const index = this.cartItems.findIndex(el => el.product.id === product.id);
+
+    let item;
+
+    if (index === -1) {
+      item = {product: product, count: 1};
+      this.cartItems.push(item);
+    } else {
+      item = this.cartItems[index];
+      ++item.count;
+    }
+
+    this.onProductUpdate(item);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    const index = this.cartItems.findIndex(el => el.product.id === productId);
+    const item = this.cartItems[index];
+
+    if (amount === 1) {
+      ++item.count;
+    } else {
+      --item.count;
+    }
+
+    if (!item.count) {
+      this.cartItems.splice(index, 1);
+    }
+
+    this.onProductUpdate(item);
   }
 
   isEmpty() {
-    // ваш код
+    return !Boolean(this.cartItems.length);
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.count * item.product.price, 0);
   }
 
   onProductUpdate(cartItem) {
